c68af326ce60f4994e3d196d67015fe1161da146
MeghanaAlluri <30402384+PrabhuLoganathan@users.noreply.github.com>
2025-07-15 20:03:36 +0530

    Update DriverFactory.java

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,9 +1,9 @@
 package utils;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,21 +17,38 @@ public class DriverFactory {
 	}
 
 	public static void setDriver() {
-        ChromeOptions options = new ChromeOptions();
+		try {
+			Path tempProfile = Files.createTempDirectory("chrome-profile");
 
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--disable-extensions");
-        options.setExperimentalOption("prefs", new HashMap<String, Object>() {{
-            put("credentials_enable_service", false);
-            put("profile.password_manager_enabled", false);
-        }});
-        
-		tlDriver.set(new ChromeDriver());
+			ChromeOptions options = new ChromeOptions();
+
+
+			options.addArguments("--disable-notifications");
+			options.addArguments("--disable-infobars");
+			options.addArguments("--disable-extensions");
+			options.addArguments("--disable-popup-blocking");
+			options.addArguments("--remote-allow-origins=*");
+
+
+			options.addArguments("--user-data-dir=" + tempProfile.toAbsolutePath().toString());
+
+
+			Map<String, Object> prefs = new HashMap<>();
+			prefs.put("credentials_enable_service", false);
+			prefs.put("profile.password_manager_enabled", false);
+			options.setExperimentalOption("prefs", prefs);
+
+			tlDriver.set(new ChromeDriver(options));
+
+		} catch (Exception e) {
+			throw new RuntimeException("Failed to initialize ChromeDriver", e);
+		}
 	}
 
 	public static void closeDriver() {
-		getDriver().quit();
-		tlDriver.remove();
+		if (getDriver() != null) {
+			getDriver().quit();
+			tlDriver.remove();
+		}
 	}
 }
